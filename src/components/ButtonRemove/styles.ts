b3042db3540b3f526bb777c4fcc0e7b6b9080c4b
adonisamarante <adonisamarante@gmail.com>
2024-07-05 16:07:02 -0300

    style: buttonRemove hover

--- src/components/ButtonRemove/styles.ts
+++ src/components/ButtonRemove/styles.ts
@@ -8,11 +8,18 @@ export const StyledButtonRemove = styled.button`
   padding: 0 0.5rem;
   background-color: ${(props) => props.theme['base-button']};
   border: none;
+  outline: none;
   border-radius: 6px;
   font-size: 0.75rem;
   line-height: 160%;
+  color: ${(props) => props.theme['base-subtitle']};
 
   .trashIcon {
     color: ${(props) => props.theme.purple};
+  }
+
+  &:hover {
+    cursor: pointer;
+    background-color: ${(props) => props.theme['base-hover']};
   }
 `
