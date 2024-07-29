05b0178abdd4cad6b8fcdc1d806e69717566dd8e
adonisamarante <adonisamarante@gmail.com>
2024-07-29 17:21:33 -0300

    feat: routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/global'
-import { CatalogCard } from './components'
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './Router'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <div>
-        <CatalogCard />
-      </div>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
 
       <GlobalStyle />
     </ThemeProvider>
