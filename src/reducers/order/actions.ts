5a35162c0d33720a22b53c292a30ea059a78dd61
adonisamarante <adonisamarante@gmail.com>
2024-08-19 19:59:40 -0300

    feat: action add coffee to order

--- src/reducers/order/actions.ts
+++ src/reducers/order/actions.ts
@@ -1,4 +1,13 @@
+import { ICoffee } from '../../infra/interfaces/coffee'
+
 export enum ActionTypes {
-  ADD_ITEM_TO_ORDER = 'ADD_ITEM_TO_ORDER',
-  REMOVE_ITEM_FROM_ORDER = 'REMOVE_ITEM_FROM_ORDER',
+  ADD_COFFEE_TO_ORDER = 'ADD_COFFEE_TO_ORDER',
+  REMOVE_COFFEE_FROM_ORDER = 'REMOVE_COFFEE_FROM_ORDER',
+}
+
+export function addCoffeeToOrderAction(coffee: ICoffee) {
+  return {
+    type: ActionTypes.ADD_COFFEE_TO_ORDER,
+    payload: { coffee },
+  }
 }
