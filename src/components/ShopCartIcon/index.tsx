3bdcb15195b3b6a77d5bae9ac4df37728ce20de1
adonisamarante <adonisamarante@gmail.com>
2024-08-21 18:24:07 -0300

    feat: shopping cart items counter working

--- src/components/ShopCartIcon/index.tsx
+++ src/components/ShopCartIcon/index.tsx
@@ -1,12 +1,18 @@
 import { ShoppingCartSimple } from 'phosphor-react'
 import { CartAmountTooltip, CartIcon } from './styles'
+import { useContext } from 'react'
+import { CoffeeOrderContext } from '../../contexts/CoffeeOrderContext'
 
 export function ShopCartIcon() {
+  const { coffees } = useContext(CoffeeOrderContext)
+
   return (
-    <CartIcon>
-      <CartAmountTooltip>
-        <span>4</span>
-      </CartAmountTooltip>
+    <CartIcon disabled={!coffees.length}>
+      {coffees.length > 0 && (
+        <CartAmountTooltip>
+          <span>{coffees.length}</span>
+        </CartAmountTooltip>
+      )}
       <ShoppingCartSimple size={22} weight="fill" className="cart-icon" />
     </CartIcon>
   )
