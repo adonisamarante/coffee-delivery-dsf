a490bc36c0ce5cbb2b08715cec6e22aa079793b9
adonisamarante <adonisamarante@gmail.com>
2024-08-30 19:34:54 -0300

    style: gradient coffee order list

--- src/pages/Checkout/styles.ts
+++ src/pages/Checkout/styles.ts
@@ -24,7 +24,8 @@ export const PaymentWrapper = styled(BaseWrapper)`
 `
 
 export const TotalOrderContainer = styled.div`
-  flex: 1;
+  width: 28rem;
+
   flex-direction: column;
   gap: 0.75rem;
 `
@@ -38,9 +39,113 @@ export const TotalOrderWrapper = styled(BaseWrapper)`
   flex-direction: column;
 `
 
+export const CoffeeListWrapper = styled.div`
+  position: relative;
+`
+
+export const CoffeeList = styled.div`
+  width: 100%;
+  max-height: 18.75rem;
+  overflow-y: scroll;
+  overflow-x: hidden;
+
+  align-items: center;
+  flex-direction: column;
+
+  scrollbar-width: none;
+  -ms-overflow-style: none;
+  ::-webkit-scrollbar {
+    display: none;
+  }
+`
+
+export const GradTop = styled.div`
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  width: 100%;
+  height: 15px;
+  pointer-events: none; /* Allows clicks to pass through the gradient */
+  background: -moz-linear-gradient(
+    top,
+    ${(props) => props.theme['base-card']} 0%,
+    rgba(137, 255, 241, 0) 100%
+  ); /* FF3.6+ */
+  background: -webkit-gradient(
+    linear,
+    left top,
+    left bottom,
+    color-stop(0%, ${(props) => props.theme['base-card']}),
+    color-stop(100%, rgba(137, 255, 241, 0))
+  ); /* Chrome,Safari4+ */
+  background: -webkit-linear-gradient(
+    top,
+    ${(props) => props.theme['base-card']} 0%,
+    rgba(137, 255, 241, 0) 100%
+  ); /* Chrome10+,Safari5.1+ */
+  background: -o-linear-gradient(
+    top,
+    ${(props) => props.theme['base-card']} 0%,
+    rgba(137, 255, 241, 0) 100%
+  ); /* Opera 11.10+ */
+  background: -ms-linear-gradient(
+    top,
+    ${(props) => props.theme['base-card']} 0%,
+    rgba(137, 255, 241, 0) 100%
+  ); /* IE10+ */
+  background: linear-gradient(
+    to bottom,
+    ${(props) => props.theme['base-card']} 0%,
+    rgba(137, 255, 241, 0) 100%
+  ); /* W3C */
+  filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#0089fff1', endColorstr='#000000',GradientType=0 ); /* IE6-9 */
+`
+
+export const GradBottom = styled.div`
+  position: absolute;
+  bottom: 0px;
+  left: 0px;
+  width: 100%;
+  height: 15px;
+  pointer-events: none; /* Allows clicks to pass through the gradient */
+  background: -moz-linear-gradient(
+    top,
+    rgba(137, 255, 241, 0) 0%,
+    ${(props) => props.theme['base-card']} 100%
+  ); /* FF3.6+ */
+  background: -webkit-gradient(
+    linear,
+    left top,
+    left bottom,
+    color-stop(0%, rgba(137, 255, 241, 0)),
+    color-stop(100%, ${(props) => props.theme['base-card']})
+  ); /* Chrome,Safari4+ */
+  background: -webkit-linear-gradient(
+    top,
+    rgba(137, 255, 241, 0) 0%,
+    ${(props) => props.theme['base-card']} 100%
+  ); /* Chrome10+,Safari5.1+ */
+  background: -o-linear-gradient(
+    top,
+    rgba(137, 255, 241, 0) 0%,
+    ${(props) => props.theme['base-card']} 100%
+  ); /* Opera 11.10+ */
+  background: -ms-linear-gradient(
+    top,
+    rgba(137, 255, 241, 0) 0%,
+    ${(props) => props.theme['base-card']} 100%
+  ); /* IE10+ */
+  background: linear-gradient(
+    to bottom,
+    rgba(137, 255, 241, 0) 0%,
+    ${(props) => props.theme['base-card']} 100%
+  ); /* W3C */
+  filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#0089fff1', endColorstr='#000000',GradientType=0 ); /* IE6-9 */
+`
+
 export const Separator = styled.div`
   width: 100%;
-  height: 1px;
+  padding: 0.03rem 0;
   background-color: ${(props) => props.theme['base-button']};
   margin: 1.5rem 0;
 `
