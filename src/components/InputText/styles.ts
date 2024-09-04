1536786a4280e03a25550bd8855e9608cb860374
adonisamarante <adonisamarante@gmail.com>
2024-09-04 17:21:19 -0300

    style: input field placeholder

--- src/components/InputText/styles.ts
+++ src/components/InputText/styles.ts
@@ -9,17 +9,29 @@ export const InputPlaceholder = styled.span<{ position: string }>`
   position: absolute;
   top: 50%;
   transform: translateY(-50%);
-  color: ${(props) => props.theme['base-label']};
   pointer-events: none;
   ${(props) => (props.position === 'start' ? 'left: 10px;' : 'right: 10px;')}
+
+  color: ${(props) => props.theme['base-label']};
+  font-size: 0.75rem;
+  font-style: italic;
 `
 
 export const InputField = styled.input`
+  width: 100%;
   background-color: ${(props) => props.theme['base-input']};
   color: ${(props) => props.theme['base-text']};
   padding: 0.75rem; /* Adjust padding to accommodate the placeholders */
-  box-sizing: border-box;
+  // box-sizing: border-box;
   border-radius: 4px;
+
+  &::placeholder {
+    color: ${(props) => props.theme['base-label']};
+    font-size: 0.875rem;
+    line-height: 130%;
+  }
+
+  border: 2px solid ${(props) => props.theme['base-button']};
 
   &:focus {
     border: 2px solid ${(props) => props.theme['yellow-dark']};
