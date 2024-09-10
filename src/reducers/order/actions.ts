851a2710ea9b3d8c3dd59922371d02cc78ab20fd
adonisamarante <adonisamarante@gmail.com>
2024-09-10 17:22:23 -0300

    feat: update coffee quantity reducer

--- src/reducers/order/actions.ts
+++ src/reducers/order/actions.ts
@@ -3,11 +3,26 @@ import { ICoffee } from '../../infra/interfaces/coffee'
 export enum ActionTypes {
   ADD_COFFEE_TO_ORDER = 'ADD_COFFEE_TO_ORDER',
   REMOVE_COFFEE_FROM_ORDER = 'REMOVE_COFFEE_FROM_ORDER',
+  UPDATE_COFFEE_ORDER_QUANTITY = 'UPDATE_COFFEE_ORDER_QUANTITY',
 }
 
 export function addCoffeeToOrderAction(coffee: ICoffee) {
   return {
     type: ActionTypes.ADD_COFFEE_TO_ORDER,
     payload: { coffee },
+  }
+}
+
+export function removeCoffeeFromOrderAction(coffeeId: string) {
+  console.log(coffeeId)
+}
+
+export function updateCoffeeQuantityAction(coffee: ICoffee, quantity: number) {
+  return {
+    type: ActionTypes.UPDATE_COFFEE_ORDER_QUANTITY,
+    payload: {
+      coffee,
+      quantity,
+    },
   }
 }
--- src/reducers/order/reducer.ts
+++ src/reducers/order/reducer.ts
@@ -1,17 +1,20 @@
 import { produce } from 'immer'
 import { IOrder } from '../../infra/interfaces/order'
 import { ActionTypes } from './actions'
+import { ICoffee } from '../../infra/interfaces/coffee'
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function orderReducer(state: IOrder, action: any) {
+  function verifyExistIndex(coffeeList: ICoffee[], payloadCoffee: ICoffee) {
+    return coffeeList.findIndex((coffee) => coffee.name === payloadCoffee.name)
+  }
+
   switch (action.type) {
     case ActionTypes.ADD_COFFEE_TO_ORDER:
       return produce(state, (draft) => {
         const payloadCoffee = action.payload.coffee
 
-        const existIndex = draft.coffees.findIndex(
-          (coffee) => coffee.name === payloadCoffee.name,
-        )
+        const existIndex = verifyExistIndex(draft.coffees, payloadCoffee)
 
         if (existIndex >= 0) {
           draft.coffees.splice(existIndex, 1, {
@@ -26,6 +29,21 @@ export function orderReducer(state: IOrder, action: any) {
 
     case ActionTypes.REMOVE_COFFEE_FROM_ORDER:
       return state
+
+    case ActionTypes.UPDATE_COFFEE_ORDER_QUANTITY:
+      return produce(state, (draft) => {
+        const payloadCoffee = action.payload.coffee
+        const quantity = action.payload.quantity
+
+        const existIndex = verifyExistIndex(draft.coffees, payloadCoffee)
+
+        if (existIndex >= 0) {
+          draft.coffees.splice(existIndex, 1, {
+            ...payloadCoffee,
+            quantity,
+          })
+        }
+      })
 
     default:
       return state
