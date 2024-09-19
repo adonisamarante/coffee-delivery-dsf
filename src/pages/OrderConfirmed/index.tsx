771bfd5c97d4bf420b0adfd6b1004f8f483edcd4
adonisamarante <adonisamarante@gmail.com>
2024-09-19 15:12:19 -0300

    feat: checkout success page preview

--- src/pages/OrderConfirmed/index.tsx
+++ src/pages/OrderConfirmed/index.tsx
@@ -1,7 +1,13 @@
+import { useContext } from 'react'
+import { CoffeeOrderContext } from '../../contexts/CoffeeOrderContext'
+
 export function OrderConfirmed() {
+  const { orderState } = useContext(CoffeeOrderContext)
+
   return (
     <>
       <span>Order Confirmed</span>
+      <pre>{JSON.stringify(orderState, null, 1)}</pre>
     </>
   )
 }
