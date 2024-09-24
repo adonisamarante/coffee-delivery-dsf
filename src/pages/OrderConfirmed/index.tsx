5d3582d298ccd596b04df4aeada0fad2a31babc8
adonisamarante <adonisamarante@gmail.com>
2024-09-24 18:52:23 -0300

    feat: success page working with real information

--- src/pages/OrderConfirmed/index.tsx
+++ src/pages/OrderConfirmed/index.tsx
@@ -1,12 +1,26 @@
 import { useContext } from 'react'
 import { CoffeeOrderContext } from '../../contexts/CoffeeOrderContext'
 import deliveryScooterImg from '../../assets/delivery-scooter.png'
-import { AddressWrapper, Container, TitleWrapper } from './styles'
+import {
+  ConfirmationInfoWrapper,
+  Container,
+  IconWrapper,
+  InfoSection,
+  InfoWrapper,
+  TitleWrapper,
+} from './styles'
+import { CurrencyDollar, MapPin, Timer } from 'phosphor-react'
+import {
+  PaymentMethod,
+  PaymentMethodDetails,
+  PaymentMethods,
+} from '../../components'
 
 export function OrderConfirmed() {
   const { orderState } = useContext(CoffeeOrderContext)
-  const orderCompleted = orderState
-  console.log(orderCompleted)
+
+  const payMethod: PaymentMethodDetails =
+    PaymentMethods[orderState.paymentMethod as PaymentMethod]
 
   return (
     <Container>
@@ -15,7 +29,43 @@ export function OrderConfirmed() {
         <span>Agora é só aguardar que logo o café chegará até você</span>
       </TitleWrapper>
       <div>
-        <AddressWrapper>address</AddressWrapper>
+        <ConfirmationInfoWrapper>
+          <InfoSection>
+            <IconWrapper bgColor="purple">
+              <MapPin weight="fill" color="white" size={16} />
+            </IconWrapper>
+            <InfoWrapper>
+              <span>
+                Entrega em{' '}
+                <span className="bold-span">
+                  {orderState.address.streetName}, {orderState.address.number}
+                </span>
+              </span>
+              <span>
+                {orderState.address.neighborhood} - {orderState.address.city},{' '}
+                {orderState.address.stateUf}
+              </span>
+            </InfoWrapper>
+          </InfoSection>
+          <InfoSection>
+            <IconWrapper bgColor="yellow">
+              <Timer weight="fill" color="white" size={16} />
+            </IconWrapper>
+            <InfoWrapper>
+              <span>Previsão de entrega</span>
+              <span className="bold-span">20 min - 30 min</span>
+            </InfoWrapper>
+          </InfoSection>
+          <InfoSection>
+            <IconWrapper bgColor="yellow-dark">
+              <CurrencyDollar weight="fill" color="white" size={16} />
+            </IconWrapper>
+            <InfoWrapper>
+              <span>Pagamento na entrega</span>
+              <span className="bold-span">{payMethod.label}</span>
+            </InfoWrapper>
+          </InfoSection>
+        </ConfirmationInfoWrapper>
         <img src={deliveryScooterImg} alt="delivery scooter" />
       </div>
     </Container>
--- src/pages/OrderConfirmed/styles.ts
+++ src/pages/OrderConfirmed/styles.ts
@@ -1,5 +1,9 @@
 import styled from 'styled-components'
 
+interface IStyleProps {
+  bgColor: string
+}
+
 export const Container = styled.div`
   flex: 1;
   flex-direction: column;
@@ -7,7 +11,7 @@ export const Container = styled.div`
 
   margin-top: 5rem;
 
-  :last-child {
+  > :last-child {
     justify-content: space-between;
     align-items: center;
   }
@@ -15,9 +19,23 @@ export const Container = styled.div`
 
 export const TitleWrapper = styled.div`
   flex-direction: column;
+
+  :first-child {
+    color: ${(props) => props.theme['yellow-dark']};
+    font-family: 'Baloo 2', sans-serif;
+    line-height: 130%;
+    font-weight: 900;
+    font-size: 2rem;
+  }
+
+  :last-child {
+    color: ${(props) => props.theme['base-subtitle']};
+    font-size: 1.25rem;
+    line-height: 130%;
+  }
 `
 
-export const AddressWrapper = styled.div`
+export const ConfirmationInfoWrapper = styled.div`
   max-width: 32.875rem;
   height: fit-content;
 
@@ -25,5 +43,36 @@ export const AddressWrapper = styled.div`
   border: 1px solid black;
   border-radius: 6px 36px 6px 36px;
 
-  justify-content: space-between;
+  flex-direction: column;
+  gap: 2rem;
+`
+
+export const InfoSection = styled.div`
+  align-items: center;
+  gap: 0.75rem;
+`
+
+export const IconWrapper = styled.div<IStyleProps>`
+  width: fit-content;
+  padding: 8px;
+  background-color: ${(props) => props.theme[props.bgColor]};
+  border-radius: 1000px;
+
+  align-items: center;
+  justify-content: center;
+`
+
+export const InfoWrapper = styled.div`
+  color: ${(props) => props.theme['base-text']};
+
+  flex-direction: column;
+
+  span {
+    font-size: 1rem;
+    line-height: 130%;
+  }
+
+  .bold-span {
+    font-weight: 700;
+  }
 `
